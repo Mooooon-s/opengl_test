a46e1a672c5f48785ada6ddc55418dba0c40c31a
Mooooon-s <64954014+Mooooon-s@users.noreply.github.com>
2022-01-12 19:31:54 +0900

    critical error

--- opengl_test/main.cpp
+++ opengl_test/main.cpp
@@ -103,21 +103,35 @@ int main()
     float R = 0.2f, G = 0.3f, B = 0.3f;
 
     float Vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f, 0.5f, 0.0f
+    -0.5f, -0.5f, 0.0f, //bottom left
+     0.5f, -0.5f, 0.0f, //top left
+     -0.5f, 0.5f, 0.0f, //bottom right
+     0.5f, 0.5f, 0.0f,   //top right
+     0.0f,-0.5f,0.0f,
+     0.0f,0.5f,0.0f,
+
     };
     
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        1,3,4,
+        0,2,5
+    };
+
+    unsigned int VBO, VAO,EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -139,7 +153,8 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -149,6 +164,7 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
 
